73356801adfffaa433f5360beff154f0fe3b395f
hudasetiawan <hudasetiawan919@gmail.com>
2024-11-11 19:32:56 +0700

    fix(firestore): add activity addCafeStore on Manifest

--- app/src/main/java/com/example/firebaseandroidproject/MainActivity.java
+++ app/src/main/java/com/example/firebaseandroidproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firebaseandroidproject;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -66,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        FloatingActionButton refresh = findViewById(R.id.refresh);
+//        FloatingActionButton refresh = findViewById(R.id.);
 //        refresh.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
